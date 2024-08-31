85a321ff8d2c1ae7dea82eb98a9bcf48a3d4888e
宝期 <66853126+baoqi-zhong@users.noreply.github.com>
2024-09-01 03:44:46 +0800

    move file

--- RM2025-Internal-Core/RM2025-Internal-Core.mk
+++ RM2025-Internal-Core/RM2025-Internal-Core.mk
@@ -1,9 +1,11 @@
 
 CPP_SOURCES += \
 $(wildcard $(CORE_DIR)/Drivers/*.cpp) \
+$(wildcard $(CORE_DIR)/Control/*.cpp)
 
 C_INCLUDES += \
--I$(CORE_DIR)/Drivers
+-I$(CORE_DIR)/Drivers \
+-I$(CORE_DIR)/Control
 
 SystemView_PATH = $(CORE_DIR)/Diagnostic/SystemView
 include $(SystemView_PATH)/SystemView.mk
